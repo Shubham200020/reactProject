0625c7c5bc4408869cfc5745d87eed15657030a8
Shubham Kumar <shubhammisra800@gmail.com>
2024-04-17 18:32:11 +0530

    Update Maping.js

--- src/Maping.js
+++ src/Maping.js
@@ -5,7 +5,7 @@ export default function Maping() {
   
   return <>
   <div>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.261317327888!2d73.91411937531447!3d18.562253982539485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1713355381493!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style={{width:'100%',height:'200vc'}}></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3867187.666169696!2d76.76983739999999!3d18.81817715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1713358571476!5m2!1sen!2sin" width="600" height="450" style={{width:'100%'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
   </>
 }
